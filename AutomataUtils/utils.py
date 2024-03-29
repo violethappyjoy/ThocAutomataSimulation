0e583968924b74b3af4f49c4f8c4cf474afa65a9
violethappyjoy <21bce026@nirmauni.ac.in>
2024-03-29 22:22:11 +0530

    Init commit 2

--- AutomataUtils/utils.py
+++ AutomataUtils/utils.py
@@ -1,3 +1,9 @@
 from abc import ABC, abstractmethod
 from functools import reduce
 from typing import TypeVar, Generic, AbstractSet, Optional, Tuple, Dict, Any, Sequence
+
+StateType = TypeVar("StateType")
+SymbolType = TypeVar("SymbolType")
+GuardType = TypeVar("GuardType")
+TransitionType = Tuple[StateType, GuardType, StateType]
+
